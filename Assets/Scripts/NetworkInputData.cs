fe222d1e4d9c044b83ea3a307db2e6a2b128d556
yuchen <yuchen@holoi.com>
2022-05-23 17:57:16 +0800

    Initial C

---
+++ Assets/Scripts/NetworkInputData.cs
@@ -0,0 +1,10 @@
+using Fusion;
+using UnityEngine;
+
+public struct NetworkInputData : INetworkInput
+{
+    public const byte MOUSEBUTTON1 = 0x01;
+
+    public byte buttons;
+    public Vector3 direction;
+}
